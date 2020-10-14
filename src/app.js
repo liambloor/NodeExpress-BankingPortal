3235338f946c79ea8804bf109bfe095f90986d90
Liam Bloor <liam.bloor@c02cw121pn5v.wren.wrenkitchens.com>
2020-10-14 08:36:57 +0100

    module1

--- src/app.js
+++ src/app.js
@@ -17,6 +17,18 @@ const users = JSON.parse(userData);
 app.get('/', function(req, response) {
     response.render('index', {title: 'Account Summary', accounts: accounts});
 });
+app.get('/savings', function(req, response) {
+    response.render('account', {account: accounts.savings});
+})
+app.get('/checking', function(req, response) {
+    response.render('account', {account: accounts.checking});
+})
+app.get('/credit', function(req, response) {
+    response.render('account', {account: accounts.credit});
+})
+app.get('/profile', function(req, res) {
+    res.render('profile', {user: users[0]});
+})
 
 app.listen(3000, function() {
     console.log(accounts)
