54375356e4e45ba50a1afb19df3911cc92f0c206
Liam Bloor <liam.bloor@c02cw121pn5v.wren.wrenkitchens.com>
2020-10-14 08:25:55 +0100

    module1

--- src/app.js
+++ src/app.js
@@ -8,11 +8,18 @@ app.use(express.static('./src/public'));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', 'utf8');
+const users = JSON.parse(userData);
+
 app.get('/', function(req, response) {
-    response.render('index', {title: 'Index'});
+    response.render('index', {title: 'Account Summary', accounts: accounts});
 });
 
 app.listen(3000, function() {
+    console.log(accounts)
     console.log("PS Project Running on port 3000!")
 });
 
