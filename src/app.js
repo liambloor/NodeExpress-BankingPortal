13c2e070e326f53d109debc718f22041ced3b7a7
Liam Bloor <liam.bloor@c02cw121pn5v.wren.wrenkitchens.com>
2020-10-14 09:59:46 +0100

    module5

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const {accounts, users, writeJSON} = require('./data')
+const {accounts, users, writeJSON} = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 const express = require('express');
 const app = new express();
 
@@ -13,36 +15,12 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, response) {
     response.render('index', {title: 'Account Summary', accounts: accounts});
 });
-app.get('/savings', function(req, response) {
-    response.render('account', {account: accounts.savings});
-});
-app.get('/checking', function(req, response) {
-    response.render('account', {account: accounts.checking});
-});
-app.get('/credit', function(req, response) {
-    response.render('account', {account: accounts.credit});
-});
 app.get('/profile', function(req, res) {
     res.render('profile', {user: users[0]});
 });
-app.get('/transfer', function(req, res) {
-    res.render('transfer');
-});
-app.post('/transfer', function(req, res) {
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('transfer', {message: "Transfer Completed"});
-});
-app.get('/payment', function(req, res) {
-   res.render('payment', {account: accounts.credit});
-});
-app.post('/payment', function(req, res) {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('payment', {message: "Payment Successful", account: accounts.credit});
-})
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.listen(3000, function() {
     console.log("PS Project Running on port 3000!");
--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,3 +1,15 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const { accounts } = require('../data.js');
+
+router.get('/savings', function(req, response) {
+    response.render('account', {account: accounts.savings});
+});
+router.get('/checking', function(req, response) {
+    response.render('account', {account: accounts.checking});
+});
+router.get('/credit', function(req, response) {
+    response.render('account', {account: accounts.credit});
+});
+
+module.exports = router;
